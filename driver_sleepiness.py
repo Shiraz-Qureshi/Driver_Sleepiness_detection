291c3fce934b8fe0454166183efbd2c79055f329
Shiraz <shirazqureshi889@gmail.com>
2023-01-26 11:45:47 +0530

    added functions

--- driver_sleepiness.py
+++ driver_sleepiness.py
@@ -26,3 +26,52 @@ def blinked(a,b,c,d,e,f):
     else:
         return 0
 
+while True:
+    _,frame=cap.read()
+    face_frame=frame.copy()
+    gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces=detector(gray)
+
+    for face in faces:
+        x1=face.left()
+        y1=face.top()
+        x2=face.right()
+        y2=face.bottom()
+        face_frame=frame.copy()
+        cv2.rectangle(face_frame,(x1,y1),(x2,y2),(0,255,0),2)
+
+        landmarks=predictor(gray,face)
+        landmarks=face_utils.shape_to_np(landmarks)
+
+        left_blink=blinked(landmarks[36],landmarks[37],landmarks[38],landmarks[41],landmarks[40],landmarks[39])
+        right_blink=blinked(landmarks[42],landmarks[43],landmarks[44],landmarks[47],landmarks[46],landmarks[45])
+
+        if(left_blink==0 or right_blink==0):
+            sleep+=1
+            drowsy=0
+            active=0
+            if(sleep>6):
+                status="SLEEPING!"
+        elif(left_blink==1 or right_blink==1):
+            sleep=0
+            active=0
+            drowsy=1
+            if(drowsy>6):
+                status="DROWSY!"
+        else:
+            sleep=0
+            drowsy=0
+            active+=1
+            if(active>6):
+                status="YOU ARE ACTIVE"
+                color=(0,255,0)
+        cv2.putText(frame,status,(100,100),cv2.FONT_HERSHEY_COMPLEX,1.2,color,3)
+        for n in range(0,68):
+            (x,y)=landmarks[n]
+            cv2.circle(face_frame,(x,y),1,(255,255,255),-1)
+    cv2.imshow("Face Detection",frame)
+    cv2.imshow("Sleep Detection",face_frame)
+    key=cv2.waitKey(1)
+    if key==27:
+        break
+
